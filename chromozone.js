f3381b4d006d53b7afa7973be1342b4dc39ff2eb
Pauline Davis <pauljdavi.t21@btinternet.com>
2023-03-01 09:08:04 +0000

    chromozone kata

---
+++ chromozone.js
@@ -0,0 +1,10 @@
+function chromosomeCheck(sperm) {
+//   return (sperm === 'XY') ? "Congratulations! You\'re going to have a son." : 
+//   "Congratulations! You\'re going to have a son.";
+//  } 
+  if (sperm === 'XY') {
+  return 'Congratulations! You\'re going to have a son.'
+  } else {
+  return 'Congratulations! You\'re going to have a daughter.'
+}
+      }
---
+++ chromozone.test.js
@@ -0,0 +1,100 @@
+
+import {chromosomeCheck} from './chromozone';
+
+
+
+// const Test = require('@codewars/test-compat');
+
+describe("Tests", () => {
+  it("test", () => {
+Test.randomize([function () {
+  Test.assertEquals(chromosomeCheck('XY'), "Congratulations! You're going to have a son.");
+}, function () {
+  Test.assertEquals(chromosomeCheck('XX'), "Congratulations! You're going to have a daughter.");
+}, function () {
+  Test.assertEquals(chromosomeCheck('XY'), "Congratulations! You're going to have a son.");
+}, function () {
+  Test.assertEquals(chromosomeCheck('XX'), "Congratulations! You're going to have a daughter.");
+}, function () {
+  Test.assertEquals(chromosomeCheck('XY'), "Congratulations! You're going to have a son.");
+}, function () {
+  Test.assertEquals(chromosomeCheck('XX'), "Congratulations! You're going to have a daughter.");
+}, function () {
+  Test.assertEquals(chromosomeCheck('XY'), "Congratulations! You're going to have a son.");
+}, function () {
+  Test.assertEquals(chromosomeCheck('XX'), "Congratulations! You're going to have a daughter.");
+}, function () {
+  Test.assertEquals(chromosomeCheck('XY'), "Congratulations! You're going to have a son.");
+}, function () {
+  Test.assertEquals(chromosomeCheck('XX'), "Congratulations! You're going to have a daughter.");
+}, function () {
+  Test.assertEquals(chromosomeCheck('XY'), "Congratulations! You're going to have a son.");
+}, function () {
+  Test.assertEquals(chromosomeCheck('XX'), "Congratulations! You're going to have a daughter.");
+}, function () {
+  Test.assertEquals(chromosomeCheck('XY'), "Congratulations! You're going to have a son.");
+}, function () {
+  Test.assertEquals(chromosomeCheck('XX'), "Congratulations! You're going to have a daughter.");
+}, function () {
+  Test.assertEquals(chromosomeCheck('XY'), "Congratulations! You're going to have a son.");
+}, function () {
+  Test.assertEquals(chromosomeCheck('XX'), "Congratulations! You're going to have a daughter.");
+}, function () {
+  Test.assertEquals(chromosomeCheck('XY'), "Congratulations! You're going to have a son.");
+}, function () {
+  Test.assertEquals(chromosomeCheck('XX'), "Congratulations! You're going to have a daughter.");
+}, function () {
+  Test.assertEquals(chromosomeCheck('XY'), "Congratulations! You're going to have a son.");
+}, function () {
+  Test.assertEquals(chromosomeCheck('XX'), "Congratulations! You're going to have a daughter.");
+}, function () {
+  Test.assertEquals(chromosomeCheck('XY'), "Congratulations! You're going to have a son.");
+}, function () {
+  Test.assertEquals(chromosomeCheck('XX'), "Congratulations! You're going to have a daughter.");
+}, function () {
+  Test.assertEquals(chromosomeCheck('XY'), "Congratulations! You're going to have a son.");
+}, function () {
+  Test.assertEquals(chromosomeCheck('XX'), "Congratulations! You're going to have a daughter.");
+}, function () {
+  Test.assertEquals(chromosomeCheck('XY'), "Congratulations! You're going to have a son.");
+}, function () {
+  Test.assertEquals(chromosomeCheck('XX'), "Congratulations! You're going to have a daughter.");
+}, function () {
+  Test.assertEquals(chromosomeCheck('XY'), "Congratulations! You're going to have a son.");
+}, function () {
+  Test.assertEquals(chromosomeCheck('XX'), "Congratulations! You're going to have a daughter.");
+}, function () {
+  Test.assertEquals(chromosomeCheck('XY'), "Congratulations! You're going to have a son.");
+}, function () {
+  Test.assertEquals(chromosomeCheck('XX'), "Congratulations! You're going to have a daughter.");
+}, function () {
+  Test.assertEquals(chromosomeCheck('XY'), "Congratulations! You're going to have a son.");
+}, function () {
+  Test.assertEquals(chromosomeCheck('XX'), "Congratulations! You're going to have a daughter.");
+}, function () {
+  Test.assertEquals(chromosomeCheck('XY'), "Congratulations! You're going to have a son.");
+}, function () {
+  Test.assertEquals(chromosomeCheck('XX'), "Congratulations! You're going to have a daughter.");
+}, function () {
+  Test.assertEquals(chromosomeCheck('XY'), "Congratulations! You're going to have a son.");
+}, function () {
+  Test.assertEquals(chromosomeCheck('XX'), "Congratulations! You're going to have a daughter.");
+}, function () {
+  Test.assertEquals(chromosomeCheck('XY'), "Congratulations! You're going to have a son.");
+}, function () {
+  Test.assertEquals(chromosomeCheck('XX'), "Congratulations! You're going to have a daughter.");
+}, function () {
+  Test.assertEquals(chromosomeCheck('XY'), "Congratulations! You're going to have a son.");
+}, function () {
+  Test.assertEquals(chromosomeCheck('XX'), "Congratulations! You're going to have a daughter.");
+}, function () {
+  Test.assertEquals(chromosomeCheck('XY'), "Congratulations! You're going to have a son.");
+}, function () {
+  Test.assertEquals(chromosomeCheck('XX'), "Congratulations! You're going to have a daughter.");
+}, function () {
+  Test.assertEquals(chromosomeCheck('XY'), "Congratulations! You're going to have a son.");
+}, function () {
+  Test.assertEquals(chromosomeCheck('XX'), "Congratulations! You're going to have a daughter.");
+}]).forEach(testCase => testCase());
+  });
+});
