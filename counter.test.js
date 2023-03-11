7bb9fdd10cee60a4e03501cfaa77505bcb97f363
Pauline Davis <pauljdavi.t21@btinternet.com>
2023-03-11 19:59:36 +0000

    add counter test

---
+++ counter.test.js
@@ -0,0 +1,19 @@
+const Test = require('@codewars/test-compat');
+
+describe('test counter', function() {
+  var counter = new Counter();
+
+  it('initialize', function() {
+    Test.assertEquals(counter.getValue(), 0, 'Initial counter value must be 0');
+  });
+  
+  it('increase', function() {
+    counter.increase();
+    Test.assertEquals(counter.getValue(), 1, 'Counter value must be increased');
+  });
+  
+  it('reset', function() {
+    counter.reset();
+    Test.assertEquals(counter.getValue(), 0, 'Counter value must be 0 after reset');
+  });
+});
