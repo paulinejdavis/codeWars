215befea3f2b810a0392b338cafb7fb7296d12d6
pauline <pauljdavi.t21@btinternet.com>
2025-07-15 12:58:08 +0100

    add daily codewars

---
+++ java/codingWars/Upper.java
@@ -0,0 +1,11 @@
+package codingWars;
+
+class Upper{
+    public static String MakeUpperCase(String str){
+        return str.toUpperCase();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(MakeUpperCase("hello"));
+    }
+}
